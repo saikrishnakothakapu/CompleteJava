301937893bd56c0faec1c28fe84c9a84ad3d7d91
kothakapu saikrishna reddy <saikrishnakothakapu@gmail.com>
2025-12-24 10:51:44 +0530

    FirstProgram - Day 1

---
+++ JavaBasics/FirstProgram.java
@@ -0,0 +1,10 @@
+
+public class FirstProgram {
+
+    public static void main(String[] args) {
+
+        //simple print statement to display "Hello, World!" on the console
+        System.out.println("Hello, World!");
+
+    }
+}

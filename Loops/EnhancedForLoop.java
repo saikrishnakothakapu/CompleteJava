aa86f8e7a343b5f9dbd77ca169b2ddeea6b57aef
kothakapu saikrishna reddy <saikrishnakothakapu@gmail.com>
2025-12-26 12:18:21 +0530

    Day-3 Updated

--- Loops/EnhancedForLoop.java
+++ Loops/EnhancedForLoop.java
@@ -6,6 +6,8 @@ public class EnhancedForLoop {
         
         int[] numbers = {1,2,3,4,5,6,7,8,9,10};
 
+        /* Enhanced for loop to iterate through the array */
+
         for(int i:numbers){
             System.out.println(i);
         }
